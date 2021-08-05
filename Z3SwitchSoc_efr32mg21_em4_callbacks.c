14e57b6dd22381acdfc82e1b61100e5198e34c6b
seb-fae <71495014+seb-fae@users.noreply.github.com>
2021-08-05 11:22:58 +0200

    Update Z3SwitchSoc_efr32mg21_em4_callbacks.c

--- Z3SwitchSoc_efr32mg21_em4_callbacks.c
+++ Z3SwitchSoc_efr32mg21_em4_callbacks.c
@@ -1,11 +1,13 @@
 
 bool emberAfPluginIdleSleepOkToSleepCallback(uint32_t durationMs)
 {
+  // Disable Watchdog to reach 5 uA of EM2 current. Otherwise EM2 current is 7.5 uA 
   WDOGn_Enable(WDOG0, false);
   return true;
 }
 bool emberAfPluginEm4EnterCallback(uint32_t* durationMs)
 {
+  // Do not go to EM4 mode if duration is not sufficient */
   if (*durationMs > 2000)
    return true;
   return false;
